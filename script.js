d953ba0a820b27e0dc1426d0f7e548cad494f194
sagar9322 <sagarkasundra223@gmail.com>
2023-05-23 12:26:16 +0530

    hidden-display

--- script.js
+++ script.js
@@ -1,66 +1,78 @@
 let number = Math.trunc(Math.random() * 20 + 1);
 var score = 20;
 let highscore = 0;
+let message = document.querySelector('.message');
+let num = document.querySelector('.number');
+let scr = document.querySelector('.score');
+let modalH1 = document.querySelector('.modal h1');
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
 
 // function call when check button will click
-
 document.querySelector('.check').addEventListener('click', function () {
     let guess = Number(document.querySelector('.guess').value);
 
     // when there is no value in input
     if (!guess) {
-        document.querySelector('.message').textContent = '⛔ Please Enter Valid Number';
-    }
-
-    // when guess less than number
-    else if (guess < number) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Number is too 👎Low';
-            score--;
-        } else {
-            document.querySelector('.message').textContent = '💥Game Over💥';
-            document.body.style.backgroundColor = 'red';
-            score = 0;
-        }
-
-    }
-
-    //when guess grater than number
-    else if (guess > number) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Number is too 👍High';
-            score--;
-        } else {
-            document.querySelector('.message').textContent = '💥Game Over💥';
-            document.body.style.backgroundColor = 'red';
-            score = 0;
-        }
-
+        message.textContent = '⛔ Please Enter Valid Number';
     }
 
     // when guess equal to number
-    else if (guess == number) {
-        document.querySelector('.message').textContent = '🎉You Won🎉';
+    else if (guess === number) {
+        message.textContent = '🎉You Won🎉';
         document.body.style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.width = '30rem';
-        document.querySelector('.number').textContent = number;
+        num.style.width = '30rem';
+        num.textContent = number;
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
         if (highscore < score) {
             highscore = score;
         }
     }
-    document.querySelector('.score').textContent = score;
+
+    // when guess is not equal to number
+    else if (guess !== number) {
+        if (guess < number && score > 1) {
+            message.textContent = 'Number is too 👎Low';
+            score--;
+        } else if (guess > number && score > 1) {
+            message.textContent = 'Number is too 👍High';
+            score--;
+        } else {
+            message.textContent = '💥Game Over💥';
+            document.body.style.backgroundColor = 'red';
+            score = 0;
+            modal.classList.remove('hidden');
+            overlay.classList.remove('hidden');
+            modalH1.textContent = '💥Game Over💥';
+            modalH1.style.fontSize = '3em';
+            modalH1.style.color = 'red';
+            document.querySelector('.highscore-display').textContent = highscore;
+        }
+
+    }
+
+
+    document.querySelector('.highscore-display').textContent = highscore;
+    scr.textContent = score;
     document.querySelector('.highscore').textContent = highscore;
 });
 
 
 // when again button will click.
 document.querySelector('.again').addEventListener('click', function () {
-    document.querySelector('.message').textContent = 'Start guessing...';
+    message.textContent = 'Start guessing...';
     score = 20;
-    document.querySelector('.score').textContent = score;
+    scr.textContent = score;
     document.body.style.backgroundColor = '#222';
-    document.querySelector('.number').textContent = '?';
+    num.textContent = '?';
     number = Math.trunc(Math.random() * 20 + 1);
-    document.querySelector('.number').style.width = '15rem';
+    num.style.width = '15rem';
     document.querySelector('.guess').value = "";
+})
+
+// when click on "X" in hidden display
+document.querySelector('.close-modal').addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 })
